ee3d49b5b3124a31cb9195fbb6ef622e77a7e907
Jordan Juel <jjuel5@gmail.com>
2020-05-21 20:11:33 -0500

    Changes for T480s

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Operator Mono:pixelsize=28:antialias=true:autohint=true";
+static char *font = "Operator Mono:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
